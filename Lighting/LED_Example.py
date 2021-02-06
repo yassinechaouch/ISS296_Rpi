d8ca28cfd5b37d8b01c328aeef723364df9d11a2
chaouchyassine <chaouchyassine@gmail.com>
2021-02-06 12:29:02 +0100

    first version

---
+++ Lighting/LED_Example.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
+from time import sleep  # Import the sleep function from the time module
+
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
+
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)  # Set pin 8 to be an output pin and set initial value to low (off)
+
+for i in range(5):
+    GPIO.output(8, GPIO.HIGH)  # Turn on
+    sleep(1)  # Sleep for 1 second
+    GPIO.output(8, GPIO.LOW)  # Turn off
+    sleep(1)  # Sleep for 1 second
+
+
+
