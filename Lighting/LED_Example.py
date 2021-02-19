1498250b66b85863e399a1d2104296cd68f976d2
yassinechaouch <chaouchyassine@gmail.com>
2021-02-19 12:36:02 +0100

    fifth version

--- Lighting/LED_Example.py
+++ Lighting/LED_Example.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 from time import sleep  # Import the sleep function from the time module
 
 GPIO.setwarnings(False)  # Ignore warning for now
-GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
+GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)  # Set pin 8 to be an output pin and set initial value to low (off)
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ISS296_Rpi
+
+GPIO Zero library: https://gpiozero.readthedocs.io/en/stable/recipes.html
