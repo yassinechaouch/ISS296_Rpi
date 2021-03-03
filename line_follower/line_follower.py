34ca050d80e916b091accccf4b850148c238b650
yassinechaouch <chaouchyassine@gmail.com>
2021-03-03 15:05:46 +0100

    update line follower code

--- line_follower/line_follower.py
+++ line_follower/line_follower.py
@@ -1,44 +1,52 @@
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
+
+rightsensor=22
+leftsensor=24
+leftmotorforward=8
+leftmotorbackward=10
+rightmotorforward=16
+rightmotorbackward=18
 
 
-GPIO.setup(2,GPIO.IN) #GPIO 2 -> Left IR out
-GPIO.setup(3,GPIO.IN) #GPIO 3 -> Right IR out
 
-GPIO.setup(4,GPIO.OUT) #GPIO 4 -> Motor 1 terminal A
-GPIO.setup(14,GPIO.OUT) #GPIO 14 -> Motor 1 terminal B
+GPIO.setup(leftsensor,GPIO.IN) #GPIO 2 -> Left IR out
+GPIO.setup(rightsensor,GPIO.IN) #GPIO 3 -> Right IR out
 
-GPIO.setup(17,GPIO.OUT) #GPIO 17 -> Motor Left terminal A
-GPIO.setup(18,GPIO.OUT) #GPIO 18 -> Motor Left terminal B
+GPIO.setup(rightmotorforward,GPIO.OUT) #GPIO 4 -> Motor right terminal A
+GPIO.setup(rightmotorbackward,GPIO.OUT) #GPIO 14 -> Motor right terminal B
+
+GPIO.setup(leftmotorforward,GPIO.OUT) #GPIO 17 -> Motor Left terminal A
+GPIO.setup(leftmotorbackward,GPIO.OUT) #GPIO 18 -> Motor Left terminal B
 
 while True:
 
-    if GPIO.input(2)==True and GPIO.input(3)==True: #both while move forward
-        GPIO.output(4,True) #1A+
-        GPIO.output(14,False) #1B-
+    if GPIO.input(rightsensor)==True and GPIO.input(leftsensor)==True: #both while move forward
+        GPIO.output(rightmotorforward,True) #1A+
+        GPIO.output(leftmotorbackward,False) #1B-
 
-        GPIO.output(17,True) #2A+
-        GPIO.output(18,False) #2B-
+        GPIO.output(leftmotorforward,True) #2A+
+        GPIO.output(rightmotorbackward,False) #2B-
 
-    elif GPIO.input(2)==False and GPIO.input(3)==True: #turn right
-        GPIO.output(4,True) #1A+
-        GPIO.output(14,True) #1B-
+    elif GPIO.input(rightsensor)==False and GPIO.input(leftsensor)==True: #turn right
+        GPIO.output(rightmotorforward,True) #1A+
+        GPIO.output(leftmotorbackward,True) #1B-
 
-        GPIO.output(17,True) #2A+
-        GPIO.output(18,False) #2B-
+        GPIO.output(leftmotorforward,True) #2A+
+        GPIO.output(rightmotorbackward,False) #2B-
 
-    elif GPIO.input(2)==True and GPIO.input(3)==False: #turn left
-        GPIO.output(4,True) #1A+
-        GPIO.output(14,False) #1B-
+    elif GPIO.input(rightsensor)==True and GPIO.input(leftsensor)==False: #turn left
+        GPIO.output(rightmotorforward,True) #1A+
+        GPIO.output(leftmotorbackward,False) #1B-
 
-        GPIO.output(17,True) #2A+
-        GPIO.output(18,True) #2B-
+        GPIO.output(leftmotorforward,True) #2A+
+        GPIO.output(rightmotorbackward,True) #2B-
 
     else:  #stay still
-        GPIO.output(4,True) #1A+
-        GPIO.output(14,True) #1B-
+        GPIO.output(rightmotorforward,True) #1A+
+        GPIO.output(leftmotorbackward,True) #1B-
 
-        GPIO.output(17,True) #2A+
-        GPIO.output(18,True) #2B-
+        GPIO.output(leftmotorforward,True) #2A+
+        GPIO.output(rightmotorbackward,True) #2B-
